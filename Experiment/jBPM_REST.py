424399de357ca048c6807b55b2b70e6079260499
Chen Z <patrickzhu1130@gmail.com>
2021-04-27 22:31:23 -0700

    Finish jBPM simulator for new BPMN

--- Experiment/jBPM_REST.py
+++ Experiment/jBPM_REST.py
@@ -1,7 +1,12 @@
 import requests
 import json
 
-container_id = "Evaluation_MI_1.0.0-SNAPSHOT/"
+# API Doc: http://localhost:8080/kie-server/docs/#/
+# Process ID is hard to find. Call endpoint to find out:
+# http://localhost:8080/kie-server/services/rest/server/containers/$$container_id$$/processes
+container_id = "Integration_Quote_Integration_V1/"
+process_id = "Integration_Quote.Integration_Quote/"
+
 general_url = "http://localhost:8080/kie-server/services/rest/server/containers/" + container_id
 sample_bearer_token = "d2JhZG1pbjp3YmFkbWlu"
 header = {
@@ -11,10 +16,12 @@ header = {
 
 
 def createProcessInstance(json_payload):
+    url = general_url + "processes/" + process_id + "instances"
     response = requests.request("POST",
-                                general_url + "processes/evaluation/instances",
+                                url,
                                 data=json_payload,
                                 headers=header)
+    #print(response, url)
     if response.status_code not in [200, 201]:
         raise Exception("createProcessInstance abnormal status_code " + str(response.status_code))
     return response.text
@@ -24,7 +31,7 @@ def retrieveActiveTasks(process_instance_id):
     response = requests.request("GET",
                                 general_url + "processes/instances/" + process_instance_id,
                                 headers=header)
-    #print('retrieveActiveTasks', response.status_code)
+    # print('retrieveActiveTasks', response.status_code)
     if response.status_code not in [200, 201]:
         raise Exception("retrieveActiveTasks abnormal status_code " + str(response.status_code))
     if response.json().get('active-user-tasks') is not None:
@@ -38,7 +45,7 @@ def startActiveTask(task_id):
     payload = ""
     response = requests.request("PUT", url, data=payload, headers=header)
 
-    #print('startActiveTask', response.status_code)
+    # print('startActiveTask', response.status_code)
     if response.status_code not in [200, 201]:
         raise Exception("startActiveTask abnormal status_code " + str(response.status_code))
 
@@ -47,7 +54,7 @@ def completeActiveTask(task_id, json_payload):
     url = general_url + "tasks/" + str(task_id) + "/states/completed"
     response = requests.request("PUT", url, data=json_payload, headers=header)
 
-    #print('completeActiveTask', response.status_code)
+    # print('completeActiveTask', response.status_code)
     if response.status_code not in [200, 201]:
         raise Exception("completeActiveTask abnormal status_code " + str(response.status_code))
 
@@ -56,6 +63,6 @@ def claimActiveTask(task_id):
     url = general_url + "tasks/" + str(task_id) + "/states/claimed"
     response = requests.request("PUT", url, data="", headers=header)
 
-    #print('claimActiveTask', response.status_code)
+    # print('claimActiveTask', response.status_code)
     if response.status_code not in [200, 201]:
         raise Exception("claimActiveTask abnormal status_code " + str(response.status_code))
